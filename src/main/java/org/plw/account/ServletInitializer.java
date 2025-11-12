0cb96c8a2c2e43063bd4d3f437854741b1a9462d
Natsumiao <natsumiao@outlook.com>
2025-11-12 13:53:59 +0800

    init

---
+++ src/main/java/org/plw/account/ServletInitializer.java
@@ -0,0 +1,13 @@
+package org.plw.account;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AccountApplication.class);
+    }
+
+}
